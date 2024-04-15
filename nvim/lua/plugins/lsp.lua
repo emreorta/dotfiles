e3d6a09cc113224a372aa2e0fc512c3a3f5dfe6c
Emre Orta <emreorta3@gmail.com>
2024-04-15 12:14:19 +0200

    chore: Clean up syntax to make it more consistent

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,62 +1,57 @@
 return {
     {
         {
-            'VonHeikemen/lsp-zero.nvim',
-            branch = 'v3.x',
+            "VonHeikemen/lsp-zero.nvim",
+            branch = "v3.x",
             lazy = true,
             config = false,
             init = function()
-                -- Disable automatic setup, we are doing it manually
                 vim.g.lsp_zero_extend_cmp = 0
                 vim.g.lsp_zero_extend_lspconfig = 0
             end,
         },
         {
-            'williamboman/mason.nvim',
+            "williamboman/mason.nvim",
             lazy = false,
             config = true,
         },
 
         -- Autocompletion
         {
-            'hrsh7th/nvim-cmp',
-            event = 'InsertEnter',
+            "hrsh7th/nvim-cmp",
+            event = "InsertEnter",
             dependencies = {
-                { 'L3MON4D3/LuaSnip' },
+                { "L3MON4D3/LuaSnip" },
             },
             config = function()
-                -- Here is where you configure the autocompletion settings.
-                local lsp_zero = require('lsp-zero')
+                local lsp_zero = require("lsp-zero")
                 lsp_zero.extend_cmp()
 
-                -- And you can configure cmp even more, if you want to.
-                local cmp = require('cmp')
+                local cmp = require("cmp")
                 local cmp_action = lsp_zero.cmp_action()
                 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
                 cmp.setup({
                     formatting = lsp_zero.cmp_format({ details = true }),
                     mapping = cmp.mapping.preset.insert({
-                        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                        ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+                        ["<C-b>"] = cmp_action.luasnip_jump_backward(),
 
-                        ['<C-,>'] = cmp.mapping.select_prev_item(cmp_select),
-                        ['<C-.>'] = cmp.mapping.select_next_item(cmp_select),
-                        ['<C-m>'] = cmp.mapping.confirm({ select = true }),
-                        ['<C-n>'] = cmp.mapping.complete(),
+                        ["<C-,>"] = cmp.mapping.select_prev_item(cmp_select),
+                        ["<C-.>"] = cmp.mapping.select_next_item(cmp_select),
+                        ["<C-m>"] = cmp.mapping.confirm({ select = true }),
+                        ["<C-n>"] = cmp.mapping.complete(),
 
-                        ['<Tab>'] = cmp_action.tab_complete(),
-                        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+                        ["<Tab>"] = cmp_action.tab_complete(),
+                        ["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
 
                         -- scroll through the documentation windows
-                        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-
-
+                        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                        ["<C-d>"] = cmp.mapping.scroll_docs(4),
                     }),
                     snippet = {
                         expand = function(args)
-                            require('luasnip').lsp_expand(args.body)
+                            require("luasnip").lsp_expand(args.body)
                         end,
                     },
                 })
@@ -65,60 +60,54 @@ return {
 
         -- LSP
         {
-            'neovim/nvim-lspconfig',
-            cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-            event = { 'BufReadPre', 'BufNewFile' },
+            "neovim/nvim-lspconfig",
+            cmd = { "LspInfo", "LspInstall", "LspStart" },
+            event = { "BufReadPre", "BufNewFile" },
             dependencies = {
-                { 'hrsh7th/cmp-nvim-lsp' },
-                { 'williamboman/mason-lspconfig.nvim' },
-                { 'nvimtools/none-ls.nvim' },
-                { 'jay-babu/mason-null-ls.nvim' },
+                { "hrsh7th/cmp-nvim-lsp" },
+                { "williamboman/mason-lspconfig.nvim" },
+                { "nvimtools/none-ls.nvim" },
+                { "jay-babu/mason-null-ls.nvim" },
             },
             config = function()
-                -- This is where all the LSP shenanigans will live
-                local lsp_zero = require('lsp-zero')
+                local lsp_zero = require("lsp-zero")
                 lsp_zero.extend_lspconfig()
 
-                --- if you want to know more about lsp-zero and mason.nvim
-                --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
                 lsp_zero.on_attach(function(client, bufnr)
-                    -- see :help lsp-zero-keybindings
-                    -- to learn the available actions
                     lsp_zero.default_keymaps({ buffer = bufnr })
 
                     local opts = { buffer = bufnr, remap = false }
-                    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
-                    vim.keymap.set('n', 'md', function() vim.lsp.buf.definition() end, opts)
-                    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-                    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-                    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-                    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
-                    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-                    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-                    vim.keymap.set('n', '<C-g>', function() vim.lsp.buf.signature_help() end, opts)
+                    vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = bufnr })
+                    vim.keymap.set("n", "md", function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                    vim.keymap.set("n", "<C-g>", function() vim.lsp.buf.signature_help() end, opts)
                 end)
 
-                require('mason-lspconfig').setup({
+                require("mason-lspconfig").setup({
                     ensure_installed = {},
                     handlers = {
                         lsp_zero.default_setup,
                         lua_ls = function()
-                            -- (Optional) Configure lua language server for neovim
                             local lua_opts = lsp_zero.nvim_lua_ls()
-                            require('lspconfig').lua_ls.setup(lua_opts)
+                            require("lspconfig").lua_ls.setup(lua_opts)
                         end,
                     }
                 })
 
-                -- black setup
-                require('mason-null-ls').setup({
+                -- lsp doesn't work for the following for some reason
+                require("mason-null-ls").setup({
                     ensure_installed = {
-                        'black',
-                        'yamlfmt'
+                        "black",
+                        "yamlfmt"
                     }
                 })
 
-                local null_ls = require('null-ls')
+                local null_ls = require("null-ls")
                 null_ls.setup({
                     sources = {
                         null_ls.builtins.formatting.black,
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,28 +1,28 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
     dependencies = {
-        'nvim-lua/plenary.nvim',
-        'Myzel394/jsonfly.nvim'
+        "nvim-lua/plenary.nvim",
+        "Myzel394/jsonfly.nvim"
     },
     config = function()
-        require('telescope').setup({})
+        require("telescope").setup({})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fws', function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+        vim.keymap.set("n", "<leader>fws", function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>fWs', function()
+        vim.keymap.set("n", "<leader>fWs", function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set("n", "<leader>ps", function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>j', "<cmd>Telescope jsonfly<CR>", {})
+        vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>j", "<cmd>Telescope jsonfly<CR>", {})
     end
 }
