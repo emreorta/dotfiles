705078b9b82ee10bbb24b7570c4270182be5965c
Emre Orta <emreorta3@gmail.com>
2024-04-17 10:12:18 +0200

    fix: Allow telescope to see hidden files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,7 +9,10 @@ return {
         require("telescope").setup({})
 
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>ff",
+            function()
+                builtin.find_files({ hidden = true, no_ignore = true })
+            end, {})
         vim.keymap.set("n", "<C-p>", builtin.git_files, {})
         vim.keymap.set("n", "<leader>fws", function()
             local word = vim.fn.expand("<cword>")
