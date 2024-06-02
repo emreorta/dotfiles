26a89c04ded18f136cb7d1da7faf3144e9016333
Emre Orta <emreorta3@gmail.com>
2024-06-02 18:13:24 +0200

    chore: Update base vim config and add locale

--- nvim/lua/emre/set.lua
+++ nvim/lua/emre/set.lua
@@ -8,6 +8,8 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -18,7 +20,7 @@ vim.opt.undodir = os.getenv("HOME") .. '/.vim/undodir'
 vim.opt.undofile = false
 
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
@@ -31,3 +33,6 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
+
+-- nvim switches language sometimes for some reason
+vim.cmd("language en_US")
