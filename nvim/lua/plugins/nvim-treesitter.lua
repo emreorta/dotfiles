25504439ac359a4131744321777978d4791decc7
Emre Orta <emreorta3@gmail.com>
2024-06-02 18:20:39 +0200

    fix: Add a treestiter keybinding to reload highlighting

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,23 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate"
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+    keys = {
+        { "<leader>rr", "<cmd>TSEnable highlight<cr>"}
     },
-
-    { "nvim-treesitter/nvim-treesitter-context" }
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+            "bash",
+            "c",
+            "json",
+            "lua",
+            "go",
+            "python",
+            "vim",
+            "vimdoc",
+            "yaml"
+        },
+    },
 }
