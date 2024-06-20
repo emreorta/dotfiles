f7158e3cc40db9d8f4defe8437680342349bfa34
Emre Orta <emreorta3@gmail.com>
2024-06-20 14:22:39 +0200

    fix: Enable fzf keybindings in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,9 @@ fish_add_path /opt/homebrew/bin
 # link python to default python3.x
 fish_add_path /opt/homebrew/opt/python/libexec/bin
 
+# enable fzf keybindings for fish
+fzf --fish | source
+
 # as a safeguard to make sure that fish works within the integrated terminal of vscode
 string match -q "$TERM_PROGRAM" "vscode" and . (code --locate-shell-integration-path fish)
 
