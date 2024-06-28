e7ac431ee4fb8874c4f1034b9a1d3ad2bec8e996
Emre Orta <emreorta3@gmail.com>
2024-06-28 14:08:56 +0200

    fix: Ensure opts is injected into treesitter config

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,10 +2,14 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+    -- opts are now passed into treesitter config properly, so manual highlight 
+    -- might not be needed anymore.
     keys = {
-        { "<leader>rr", "<cmd>TSEnable highlight<cr>"}
+        { "<leader>rr", "<cmd>TSEnable highlight<cr>" }
     },
+    main = "nvim-treesitter.configs",
     opts = {
+        auto_install = { enable = true },
         highlight = { enable = true },
         indent = { enable = true },
         ensure_installed = {
