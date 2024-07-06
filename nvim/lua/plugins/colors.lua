78fbee3983cd61aee50ffecb0f7ae31b1ba04b2d
Emre Orta <emreorta3@gmail.com>
2024-07-06 19:57:03 +0200

    chore: Add new colorschemes

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,32 +1,42 @@
 function SetColorscheme(color)
-    color = color or os.getenv("NVIM_COLORSCHEME") or "nightfox"
-    vim.cmd.colorscheme(color)
+  color = color or os.getenv("NVIM_COLORSCHEME") or "nightfox"
+  vim.cmd.colorscheme(color)
 end
 
 return {
-    {
-        "tjdevries/colorbuddy.nvim",
-    },
+  {
+    "tjdevries/colorbuddy.nvim",
+  },
 
-    -- favorites
-    {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            require('kanagawa').setup({
-                commentStyle = { italic = false },
-                functionStyle = { italic = false },
-                keywordStyle = { italic = false },
-                transparent = false,
-                terminalColors = true,
-            })
+  -- alternatives
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require('kanagawa').setup({
+        commentStyle = { italic = false },
+        functionStyle = { italic = false },
+        keywordStyle = { italic = false },
+        transparent = false,
+        terminalColors = true,
+      })
 
-            SetColorscheme()
-        end
-    },
-    { "EdenEast/nightfox.nvim" },
-    { "morhetz/gruvbox" },
-
-    -- also not bad
-    { "tomasiser/vim-code-dark" },
-    { "sainnhe/everforest" },
+      SetColorscheme()
+    end
+  },
+  { "EdenEast/nightfox.nvim" },
+  { "morhetz/gruvbox" },
+  { "habamax/vim-gruvbit" },
+  {
+    'ribru17/bamboo.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('bamboo').setup {
+        -- optional configuration here
+      }
+      require('bamboo').load()
+    end,
+  },
+  { "tomasiser/vim-code-dark" },
+  { "sainnhe/everforest" },
 }
