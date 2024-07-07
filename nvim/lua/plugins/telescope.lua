5c867d917cf031de4a59c820e3dbb72be9e3c02a
Emre Orta <emreorta3@gmail.com>
2024-07-08 00:04:13 +0200

    feat: Include untracked files in search
      - feat: Enabled `show_tracked` to include untracked file in search.
      - chore: Clean up the syntax a bit

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,44 +1,52 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "Myzel394/jsonfly.nvim"
-    },
-    config = function()
-        require("telescope").setup({
-            defaults = {
-                vimgrep_arguments = {
-                    "rg",
-                    "--with-filename",
-                    "--line-number",
-                    "--column",
-                    "--smart-case",
-                    "--trim",
-                    "--ignore-file",
-                    ".gitignore"
-                }
-            }
-        })
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.6",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "Myzel394/jsonfly.nvim"
+  },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--trim",
+          "--ignore-file",
+          ".gitignore"
+        }
+      }
+    })
 
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff",
-            function()
-                builtin.find_files({ hidden = true })
-            end, {})
-        vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>fws", function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set("n", "<leader>fWs", function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set("n", "<leader>ps", function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-        vim.keymap.set("n", "<leader>j", "<cmd>Telescope jsonfly<CR>", {})
-    end
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff",
+      function()
+        builtin.find_files({ hidden = true })
+      end, {})
+
+    vim.keymap.set("n", "<leader>fp",
+      function()
+        builtin.git_files({ show_untracked = true })
+      end, {})
+
+    vim.keymap.set("n", "<leader>fws", function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word })
+    end)
+
+    vim.keymap.set("n", "<leader>fWs", function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({ search = word })
+    end)
+
+    vim.keymap.set("n", "<leader>ps", function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+
+    vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>j", "<cmd>Telescope jsonfly<CR>", {})
+  end
 }
