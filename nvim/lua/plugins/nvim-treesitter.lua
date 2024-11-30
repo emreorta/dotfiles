4baa4ac577575f46be3cdbcea13a9fd56a5f4da0
Emre Orta <emreorta3@gmail.com>
2024-11-30 11:05:50 +0100

    chore: Remove keybinding for manual TS highlighting

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,11 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   dependencies = { "nvim-treesitter/nvim-treesitter-context" },
-  -- opts are now passed into treesitter config properly, so manual highlight
-  -- might not be needed anymore.
-  keys = {
-    { "<leader>rr", "<cmd>TSEnable highlight<cr>" }
-  },
   main = "nvim-treesitter.configs",
   opts = {
     auto_install = { enable = true },
