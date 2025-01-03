165617212091055f6f615ad24c596a23f11994da
Emre Orta <emreorta3@gmail.com>
2025-01-03 17:34:50 +0100

    feat: Add new keymaps and update some of the existing ones

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 
 -- open netrw
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>mv", vim.cmd.Ex)
 
 -- move line(s) up and down in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -10,6 +10,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- apply the following commands but center view afterwards
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set({ "n", "v" }, "G", "Gzz")
+vim.keymap.set({ "n", "v" }, "gg", "ggzz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -37,7 +39,7 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- replace the word under cursor globally
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
 
 -- replace the word under cursor in the same line only
 vim.keymap.set("n", "<leader>ls", [[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -58,7 +60,7 @@ vim.keymap.set("t", "<C-k>", "<cmd>wincmd <C-w>k<CR>")
 vim.keymap.set("t", "<C-l>", "<cmd>wincmd <C-w>l<CR>")
 
 -- allows using tab to cycle between brackets, curly brackets, parentheses etc.
-vim.keymap.set("n", "<Tab>", "%")
+vim.keymap.set({ "n", "v" }, "<Tab>", "%")
 
 -- adds`if err != nil` in the line below
 vim.keymap.set(
@@ -84,3 +86,24 @@ vim.keymap.set(
 vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
 end)
+
+-- duplicate the line and comment out the previous one
+vim.keymap.set("n", "yc", "yygccp", { remap = true })
+vim.keymap.set("v", "<leader>C", "ygvgc`>p", { remap = true })
+
+-- i got tired of typing :noh
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- copy the file contents
+vim.keymap.set({ "n", "v" }, "<leader>yf", "<cmd>%y<CR>")
+
+-- send current line to `sh` and replace with the output
+vim.keymap.set("n" , "<C-s><C-s>" , ":.!sh<cr>" , { noremap = true })
+
+-- block insert in line visual mode
+vim.keymap.set("x", "A", function() return vim.fn.mode() == "V" and "$<C-v>A" or "A" end, { expr = true })
+vim.keymap.set("x", "I", function() return vim.fn.mode() == "V" and "^<C-v>I" or "I" end, { expr = true })
+
+-- for ergonomics
+vim.keymap.set("n", "<leader>;w", ":w<CR>")
+vim.keymap.set("n", "<leader>;q", ":q<CR>")
