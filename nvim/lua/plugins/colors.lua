82c25d4fda9267fb68acd806342dc6a9d823835f
Emre Orta <github@emreorta.com>
2025-05-16 09:47:08 +0200

    chore: Update colorschemes

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,4 +1,5 @@
 return {
-  { "EdenEast/nightfox.nvim",          lazy = false,     priority = 1000 },
-  { "webhooked/kanso.nvim",            lazy = false,     priority = 1000 },
+  { "EdenEast/nightfox.nvim",           lazy = false, priority = 1000 },
+  { "webhooked/kanso.nvim",             lazy = false, priority = 1000 },
+  { "aktersnurra/no-clown-fiesta.nvim", lazy = false, priority = 1000 }
 }
