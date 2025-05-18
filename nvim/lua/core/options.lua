5bdc0c8d4d2307a9909be060a649756780833e6b
Emre Orta <github@emreorta.com>
2025-05-18 12:07:44 +0200

    refactor: Tinker with options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,6 +1,9 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25-Cursor-blinkon250,r-cr-o:hor20"
 
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
@@ -11,8 +14,6 @@ vim.opt.expandtab = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.smartindent = true
-
-vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -31,13 +32,22 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "88"
 
-vim.g.mapleader = " "
-
-vim.opt.wrap = true
+vim.opt.wrap = false
 vim.opt.linebreak = true
 
 vim.g.python3_host_prog = os.getenv("VIRTUALENVS_HOME") .. "/neovim/bin/python"
 
-vim.opt.jumpoptions = "stack,view"
+vim.opt.jumpoptions = "view"
 
 vim.opt.foldlevelstart = 99
+vim.opt.foldcolumn = "1"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+
+vim.opt.grepformat = "%f:%l:%c:%m"
+vim.opt.grepprg = "rg --vimgrep"
+
+vim.opt.list = true
+vim.opt.completeopt = "menu,menuone,noselect"
