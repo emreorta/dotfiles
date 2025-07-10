52e77f0860f892eea7164073ff380e23b96db590
Emre Orta <github@emreorta.com>
2025-07-10 16:52:41 +0200

    refactor: Tweak `lua_ls` config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -102,8 +102,15 @@ return {
         handlers = {
           lsp_zero.default_setup,
           lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require("lspconfig").lua_ls.setup(lua_opts)
+            require("lspconfig").lua_ls.setup {
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim" }
+                  }
+                }
+              }
+            }
           end,
 
           pyright = function()
@@ -152,4 +159,3 @@ return {
     end,
   }
 }
-
