ea4df5c2112bcf758b4f5f92b689dba5e4a26396
Emre Orta <github@emreorta.com>
2025-08-05 11:12:51 +0200

    chore: Add keymaps for navigating to start and end of a line

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -22,6 +22,8 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "gl", "$")
+vim.keymap.set("n", "gh", "0")
 
 -- repeat the last macro
 vim.keymap.set("n", ",", "@@")
