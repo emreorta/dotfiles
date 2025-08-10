5ae6a455d63aef939226a92be68f567569ad0e25
Emre Orta <github@emreorta.com>
2025-08-10 10:55:04 +0200

    feat: Tweak telescope config
      - feat: Add navigation and editing specific mappings.
      - feat: Use `fd` in `find_files`.
      - feat: Use `--no-heading` with `rg`.
      - feat: Use `fzf` for search.
      - feat: Use a custom previewer to handle the preview of binary files.
      - chore: Bump version to `0.1.8`.
      - chore: Update packages.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,16 +1,75 @@
+local utils = {}
+
+function utils.buffer_previewer_maker(filepath, bufnr, opts)
+  local previewers = require("telescope.previewers")
+  local Job = require("plenary.job")
+
+  filepath = vim.fn.expand(filepath)
+  Job:new({
+    command = "file",
+    args = { "--mime-type", "-b", filepath },
+    on_exit = function(j)
+      local mime_type = vim.split(j:result()[1], "/")[1]
+      if mime_type == "text" then
+        previewers.buffer_previewer_maker(filepath, bufnr, opts)
+      else
+        -- just output BINARY in the preview instead
+        vim.schedule(function()
+          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+        end)
+      end
+    end
+  }):sync()
+end
+
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.6",
+  event = "VeryLazy",
+  tag = "0.1.8",
   dependencies = {
-    { "nvim-lua/plenary.nvim", lazy = true },
-    { "Myzel394/jsonfly.nvim", lazy = true }
+    { "nvim-lua/plenary.nvim",                    lazy = true },
+    { "Myzel394/jsonfly.nvim",                    lazy = true },
+    { 'nvim-telescope/telescope-fzf-native.nvim', lazy = true, build = 'make' }
   },
   config = function()
+    local actions = require("telescope.actions")
+    local action_layout = require("telescope.actions.layout")
+
     require("telescope").setup({
       defaults = {
+        buffer_previewer_maker = utils.buffer_previewer_maker,
+        mappings = {
+          i = {
+            ["<C-u>"] = false,
+            ["<C-d>"] = actions.delete_buffer + actions.move_to_top,
+            ["<C-s>"] = actions.cycle_previewers_next,
+            ["<C-a>"] = actions.cycle_previewers_prev,
+            ["<M-p>"] = action_layout.toggle_preview
+          },
+          n = {
+            ["<M-p>"] = action_layout.toggle_preview
+          },
+        },
+        pickers = {
+          find_files = {
+            find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+            mappings = {
+              n = {
+                ["cd"] = function(prompt_bufnr)
+                  local selection = require("telescope.actions.state").get_selected_entry()
+                  local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+
+                  actions.close(prompt_bufnr)
+                  vim.cmd(string.format("silent lcd %s", dir))
+                end
+              }
+            }
+          },
+        },
         vimgrep_arguments = {
           "rg",
           "--with-filename",
+          "--no-heading",
           "--line-number",
           "--column",
           "--smart-case",
@@ -19,9 +78,11 @@ return {
           ".gitignore",
           "--hidden",
           "--glob=!.git/*"
-        }
+        },
       }
     })
+
+    require("telescope").load_extension("fzf")
 
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff",
