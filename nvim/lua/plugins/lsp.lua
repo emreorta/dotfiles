87ab04086882f5ac1e9af2d51b963b98ca19b065
Emre Orta <github@emreorta.com>
2025-08-14 17:06:23 +0200

    feat: Update Mason ui config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,9 +10,18 @@ return {
     end,
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     lazy = false,
     config = true,
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    }
   },
   { "onsails/lspkind.nvim" },
 
