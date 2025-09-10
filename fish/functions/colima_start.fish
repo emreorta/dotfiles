b79c4603abe592e6d9037c5c21f244fe30c57859
Emre Orta <github@emreorta.com>
2025-09-10 20:26:41 +0200

    perf: Tweak colima config

--- fish/functions/colima_start.fish
+++ fish/functions/colima_start.fish
@@ -1,3 +1,3 @@
 function colima_start 
-    colima start --arch aarch64 --vm-type=vz --vz-rosetta --cpu 8 --memory 16 --disk 60 
+    colima start --arch aarch64 --vm-type=vz --vz-rosetta --cpu 8 --memory 16 --disk 120
 end
