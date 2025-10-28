28adc6f34efeefc032404b286c4473d0f693fab9
Emre Orta <github@emreorta.com>
2025-10-28 15:03:49 +0100

    refactor: Move abbreviations under their own config

---
+++ fish/conf.d/abbr.fish
@@ -0,0 +1,14 @@
+abbr -a -- k "kubectl"
+abbr -a -- vim "nvim"
+abbr -a -- du "dust"
+abbr -a -- find "fd"
+
+# git stuff
+abbr -a -- gco "git checkout"
+abbr -a -- gcob "git checkout -b"
+abbr -a -- gcom "git checkout main"
+abbr -a -- gplo "git pull origin"
+abbr -a -- gpsf "git push --force-with-lease"
+abbr -a -- grpo "git remote prune origin"
+abbr -a -- grbi "git rebase -i HEAD~"
+abbr -a -- gbd "git branch -D"
--- fish/config.fish
+++ fish/config.fish
@@ -13,9 +13,6 @@ set -gx EDITOR nvim
 # NVIM_COLORSCHEME is used in ../nvim/lua/plugins/colors.lua for colorscheme
 set -gx NVIM_COLORSCHEME "kanso-ink"
 
-# useful aliases
-alias vim="nvim"
-
 # enable fzf, zoxide, pyenv keybindings
 fzf --fish | source
 zoxide init fish | source
@@ -26,17 +23,3 @@ set -gx ATUIN_NOBIND "true"
 atuin init fish --disable-up-arrow | source
 bind \cr _atuin_search
 bind -M insert \cr _atuin_search
-
-# abbreviations
-# git stuff
-abbr -a -- gco "git checkout"
-abbr -a -- gcob "git checkout -b"
-abbr -a -- gcom "git checkout main"
-abbr -a -- gplo "git pull origin"
-abbr -a -- gpsf "git push --force-with-lease"
-abbr -a -- grpo "git remote prune origin"
-abbr -a -- grbi "git rebase -i HEAD~"
-abbr -a -- gbd "git branch -D"
-
-# k8s stuff
-abbr -a -- k "kubectl"
