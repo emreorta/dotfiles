09e3d79b068616a3cdd6631b111750376b30de93
Emre Orta <github@emreorta.com>
2025-10-28 15:04:18 +0100

    chore: Add keymap for `:Git pull` in `fugitive`

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -4,5 +4,6 @@ return {
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
     vim.keymap.set("n", "<leader>gl", function() vim.cmd.Git("log") end)
     vim.keymap.set("n", "<leader>gw", vim.cmd.Gwrite)
+    vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git("pull") end)
   end
 }
