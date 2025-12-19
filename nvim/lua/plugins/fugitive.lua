475b698ba1925ca64d8dd7447b066bd97a0b32f4
Emre Orta <github@emreorta.com>
2025-12-19 14:02:31 +0100

    chore: Tweak fugitive keymaps

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -4,6 +4,7 @@ return {
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
     vim.keymap.set("n", "<leader>gl", function() vim.cmd.Git("log") end)
     vim.keymap.set("n", "<leader>gw", vim.cmd.Gwrite)
-    vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git("pull") end)
+    vim.keymap.set("n", "<leader>gpl", function() vim.cmd.Git("pull") end)
+    vim.keymap.set("n", "<leader>gpsf", function() vim.cmd.Git("push --force-with-lease") end)
   end
 }
