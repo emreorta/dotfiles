48f3af6449673897c647a961fc625c92e639f2cb
Emre Orta <github@emreorta.com>
2025-12-22 15:39:19 +0100

    refactor: Change the default behavior of `bat` with `cat`
      - refactor: Change the default behavior to hide the number row.
      - feat: Add `-n|--number` to output the number row.

--- fish/functions/cat.fish
+++ fish/functions/cat.fish
@@ -1,6 +1,29 @@
-function cat
+function cat -d "Replace `cat` with `bat` with fallback"
+    argparse h/help n/number -- $argv
+
+    if set -ql _flag_help
+        echo "Usage: cat [-h|--help] [-p|--plain]
+
+        Replace `cat` with `bat` with `cat` being the fallback.
+
+        It defaults to `bat --style=plain --paging=never` and allows passing
+        `-n` or `--number` to output the number row.
+
+        Options:
+          -h, --help    Show this help message and exit.
+          -n, --number   Use `bat` with `--number`.
+        "
+      return 0
+    end
+
     if type -q bat
-        command bat --style=plain --paging=never --number $argv
+        set bat_opts --style=plain --paging=never
+
+        if set -ql _flag_number
+          set bat_opts $bat_opts --number
+        end
+
+        command bat $bat_opts $argv
     else
         command cat $argv
     end
