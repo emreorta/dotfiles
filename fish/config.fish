0d71167f92e8de3ee9f6b64917c08ac4c68262b2
Emre Orta <github@emreorta.com>
2026-02-21 20:44:17 +0100

    fix: Force Firefox and rofi to use x11
    rofi has been causing issues with wayland lately and it's been quite
    unstable, causing lots of crashes (especially with browsers). forcing
    x11 seems fix the issue.

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,16 @@
 fish_add_path $(which fish)
 fish_add_path $(which python3)
 
+# python stuff
 set -gx PYTHONDONTWRITEBYTECODE True
 set -gx PIP_REQUIRE_VIRTUALENV False
 set -gx VIRTUALENVS_HOME $HOME/.virtualenvs
 set -gx PIP_DOWNLOAD_CACHE $HOME/.pip/cache
+
+# force firefox to use x11 instead
+set -gx MOZ_ENABLE_WAYLAND 0
+
+set -gx TERMINAL alacritty
 
 # nvim > vim
 set -gx EDITOR nvim
