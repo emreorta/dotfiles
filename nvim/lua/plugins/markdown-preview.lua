789c28a18c6ddd0f1d0bf156726b0fa93bbb4861
Emre Orta <github@emreorta.com>
2026-03-24 09:34:48 +0100

    fix: Set up `markdown-preiew` properly

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,16 +1,17 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    keys = {
-      {
-        "<leader>mpt",
-        ft = "markdown",
-        "<cmd>MarkdownPreviewToggle<CR>",
-        desc = "Markdown Preview"
-      }
-    }
-  }
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && npm install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+  keys = {
+    {
+      "<leader>mpt",
+      ft = "markdown",
+      "<cmd>MarkdownPreviewToggle<CR>",
+      desc = "Markdown Preview",
+    },
+  },
 }
