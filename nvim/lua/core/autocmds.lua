1f337e5aa57cf3090b5de468e78af3a6d5aa61d8
Emre Orta <github@emreorta.com>
2026-04-11 13:24:00 +0200

    refactor: Migrate nvim-treesitter to `main` branch

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -11,11 +11,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- uses treesitter for folding if there's a parser available for that language
 vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
-    if require("nvim-treesitter.parsers").has_parser() then
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    if pcall(vim.treesitter.get_parser) then
+      vim.wo[0][0].foldmethod = "expr"
+      vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
     else
-      vim.opt.foldmethod = "syntax"
+      vim.wo[0][0].foldmethod = "syntax"
     end
   end,
 })
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,34 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  main = "nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
   dependencies = { "nvim-treesitter/nvim-treesitter-context" },
-  branch = "master",
-  main = "nvim-treesitter.configs",
-  opts = {
-    auto_install = { enable = true },
-    highlight = { enable = true },
-    indent = { enable = true },
-    ensure_installed = {
-      "c",
-      "lua",
-      "vim",
-      "vimdoc",
-      "query"
-    },
-  },
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
+  config = function()
+    local parsers = {
+      "dockerfile",
+      -- used by git
+      "diff", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
+      -- comes with nvim by default
+      "c", "lua", "query", "vim", "vimdoc",
+      "python",
+      "markdown", "markdown_inline",
+      -- other
+      "make", "proto", "regex", "ssh_config", "tmux",
+    }
+    local installed = require("nvim-treesitter.config").get_installed()
+    local to_install = vim.iter(parsers)
+        :filter(function(p) return not vim.tbl_contains(installed, p) end)
+        :totable()
+    require("nvim-treesitter").install(to_install)
+  end,
 }
