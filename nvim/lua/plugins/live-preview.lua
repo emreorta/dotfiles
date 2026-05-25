347872b1a89df1c7ff3107ff42c5f971591f1f2a
Emre Orta <github@emreorta.com>
2026-05-25 20:20:04 +0200

    refactor: Migrate to `live-preview.nvim`

---
+++ nvim/lua/plugins/live-preview.lua
@@ -0,0 +1,12 @@
+return {
+  "brianhuster/live-preview.nvim",
+  dependencies = {
+    "ibhagwan/fzf-lua",
+  },
+  lazy = true,
+  config = function()
+    vim.keymap.set("n", "<leader>ls", "<cmd>LivePreview start<CR>")
+    vim.keymap.set("n", "<leader>lc", "<cmd>LivePreview close<CR>")
+    vim.keymap.set("n", "<leader>lp", "<cmd>LivePreview pick<CR>")
+  end,
+}
