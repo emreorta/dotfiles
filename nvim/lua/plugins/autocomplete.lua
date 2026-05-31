7377644577dd55eea845f448340d6354c392248c
Emre Orta <github@emreorta.com>
2026-05-31 10:59:45 +0200

    fix(style): Clean up formatting

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -1,11 +1,11 @@
 return {
   "saghen/blink.cmp",
-  dependencies = { 
+  dependencies = {
     "saghen/blink.lib",
-    "rafamadriz/friendly-snippets"
+    "rafamadriz/friendly-snippets",
   },
   build = function()
-    require('blink.cmp').build():wait(60000)
+    require("blink.cmp").build():wait(60000)
   end,
   opts = {
     keymap = {
