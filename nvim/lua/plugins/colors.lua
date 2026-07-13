c2c2328d3531e57e243fa8822a414baa088be85e
Emre Orta <github@emreorta.com>
2026-07-13 12:59:33 +0200

    chore: Update colorschemes

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,5 +1,6 @@
 return {
   { "EdenEast/nightfox.nvim",           lazy = false, priority = 1000 },
   { "webhooked/kanso.nvim",             lazy = false, priority = 1000 },
-  { "aktersnurra/no-clown-fiesta.nvim", lazy = false, priority = 1000 }
+  { "aktersnurra/no-clown-fiesta.nvim", lazy = false, priority = 1000 },
+  { "mcauley-penney/ice-cave.nvim",     lazy = false, priority = 1000 },
 }
